4c8f9c2098cc89e0d432192f2b50fd35cdd74b3c
piy-bebe <piy.bebe@gmail.com>
2026-09-22 12:29:54 +0300

    Сделал функционал с кнопкой

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log('Привет как дела?');
+const btn = document.querySelector('#a');
+
+btn.addEventListener('click', () => {
+  const body = document.querySelector('body');
+  body.style.backgroundColor = 'black';
+});
