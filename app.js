5bce8e9a8907881164d8ab0e0e903d9d4426411e
piy-bebe <piy.bebe@gmail.com>
2026-09-22 12:41:34 +0300

    fix

--- app.js
+++ app.js
@@ -3,6 +3,5 @@ const btn = document.querySelector('#a');
 
 btn.addEventListener('click', () => {
   const body = document.querySelector('body');
-  body.style.backgroundColor = 'green';
-  body.style.fontSize = '90px';
+  body.style.backgroundColor = 'black';
 });
