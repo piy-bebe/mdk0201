af29f708d269c82f72799a324f2f1a5f905a8688
piy-bebe <piy.bebe@gmail.com>
2026-09-22 12:46:04 +0300

    fix: исправить цвет фона при выключении

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const btn = document.querySelector('#a');
 
 btn.addEventListener('click', () => {
   const body = document.querySelector('body');
-  body.style.backgroundColor = 'green';
+  body.style.backgroundColor = 'black';
 });
